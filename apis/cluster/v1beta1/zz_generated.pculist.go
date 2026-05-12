08033a571d4e35287fed69ffb8bbbf823ca35f78
kisahm <kim@cast.ai>
2026-05-12 16:35:00 +0200

    Fix GetItems return type to match resource.ProviderConfigUsageList interface
    The interface expects []resource.ProviderConfigUsage but we were returning
    []ProviderConfigUsage. Added conversion loop to return pointers to the
    concrete type as the interface type.
    
    Co-Authored-By: Kimchi <noreply@kimchi.dev>

--- apis/cluster/v1beta1/zz_generated.pculist.go
+++ apis/cluster/v1beta1/zz_generated.pculist.go
@@ -5,7 +5,13 @@ Copyright 2022 Upbound Inc.
 
 package v1beta1
 
+import resource "github.com/crossplane/crossplane-runtime/v2/pkg/resource"
+
 // GetItems of this ProviderConfigUsageList.
-func (p *ProviderConfigUsageList) GetItems() []ProviderConfigUsage {
-	return p.Items
+func (p *ProviderConfigUsageList) GetItems() []resource.ProviderConfigUsage {
+	items := make([]resource.ProviderConfigUsage, len(p.Items))
+	for i := range p.Items {
+		items[i] = &p.Items[i]
+	}
+	return items
 }
